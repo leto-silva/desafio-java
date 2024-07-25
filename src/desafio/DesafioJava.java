da67dbcef02d05b48fb680e98579e6e4fa17c7d7
WellingtonEugenio <wellingtoneugenio@gmail.com>
2024-07-25 16:15:22 -0300

    Classes desafio e ParametrosInvalidosException

--- src/desafio/DesafioJava.java
+++ src/desafio/DesafioJava.java
@@ -1,9 +1,46 @@
 package desafio;
 
+import java.util.Scanner;
+
 public class DesafioJava {
 	
 	public static void main(String[] args) {
+		//
+		int parametroUm, parametroDois;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		parametroUm = scanner.nextInt();
+		parametroDois = scanner.nextInt();
+		
+		try {
+			contar(parametroUm, parametroDois);
+		} catch (ParametrosInvalidosException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
 		
 	}
+	
+	
+	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+		
+		if (parametroUm > parametroDois) {			
+			throw new ParametrosInvalidosException("Parâmetros incorretos, Verifique!");
+		}
+		
+		int intervalo = parametroDois - parametroUm;
+		
+		for (int i = 0; i <= intervalo; i++) {
+			System.out.println(i);
+		}
+		
+		
+	}
+
+
 
 }
